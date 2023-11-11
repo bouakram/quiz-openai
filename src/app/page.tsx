f09abe4c28d2fb1114d242320a85cd3762cc2067
Akrem Boughazi <kikoua203@gmail.com>
2023-11-11 02:50:52 +0100

    adding main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,26 @@
-export default function Home() {
+import SignInButton from "@/components/SignInButton";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { getAuthSession } from "@/lib/nextAuth";
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+  const session = await getAuthSession()
+  if (session?.user) {
+    redirect('/home')
+  }
   return (
-    <main className="">
+    <main className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+      <Card className="w-[400px] bg-gray-100 dark:bg-gray-900">
+        <CardHeader>
+          <CardTitle className="text-center text-lg font-bold text-gray-950 dark:text-gray-50">QuizGen: AI-Powered Quiz Generator 🤖</CardTitle>
+        </CardHeader>
+        <CardContent className="text-center text-sm font-medium text-gray-800 dark:text-gray-200">
+          <p>Create engaging quizzes effortlessly with QuizGen, an innovative app powered by OpenAI. Generate dynamic and interactive quizzes for your audience with ease</p>
+        </CardContent>
+        <CardFooter className="flex items-center justify-center">
+          <SignInButton text={"Sign In with Google."} />
+        </CardFooter>
+      </Card>
     </main>
   )
 }
